b72889437d8f1d272c92c5b420ee4d995f8eb4ad
Denny Wong <denwong47@hotmail.com>
2024-03-16 11:25:16 +0000

    ci: Added install Protoc step. doc: Added more technical explanations to `Worker` struct and the library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
 //! Ants library.
 //!
+//! This library provides a simple way to create a worker pool to handle tasks.
+//! It assumes that the tasks are CPU bound and that the workers are stateless.
+//!
+//! Built on top of the [`tokio`] runtime and [`tonic`] for
+//! communication among the workers, this library does not require a leader
+//! or a coordinator to manage the workers. Each worker is capable of accepting
+//! tasks and processing them independently, or forwarding them to another
+//! worker should it be busy.
+//!
+//! It is anticipated that each worker will host their own Axum server to
+//! receive tasks and return the results, but this library does not enforce
+//! that. It is up to the user to decide how to take tasks and do work, while
+//! the library provides the means to relocate tasks among workers.
+//!
+//! It is not required for any data transferred to be in JSON, but it must be
+//! serializable and deserializable into a string. Technically, base64 encoded
+//! binary data can be used.
+//!
+//! See `bin/serve.rs` for an example of how to use this library to host a
+//! server to listen and do work in ~100 lines of code.
 
 mod config;
 pub use config::CliArgs;
--- src/worker/model.rs
+++ src/worker/model.rs
@@ -30,7 +30,34 @@ use crate::AntsError;
 static RESERVE_TIMEOUT: std::time::Duration = std::time::Duration::from_millis(500);
 
 const RESERVE_ATTEMPTS: usize = 32;
+
 /// A worker that can do work or distribute work.
+///
+/// A unit of work is defined by an async function that takes a type `T` and returns
+/// a `Result<R, E>`. All workers are expected to be stateless, and work performed
+/// by any worker is expected to be the same as any other worker.
+///
+/// The work is also expected to be blocking for the duration of the work, and
+/// no concurrent work can be performed. This is usually the case when the work
+/// involves physical devices such as GPU, or the work being extremely CPU
+/// bound, rendering concurrent work to be ineffective.
+///
+/// The worker should be instantiated with a list of nodes that it can forward
+/// work to, alongside the aforementioned function. The worker will then listen
+/// on gRPC for work requests. Any calls to [`find_worker_and_work`] will
+/// attempt to reserve the worker, and do work if possible, or forward the work
+/// to another worker if not.
+///
+/// During the negotiation of work with another worker, the worker will attempt
+/// to reserve the other worker before forwarding the work. If the other worker
+/// is reserved, the worker will attempt to reserve the next worker in the list.
+///
+/// Reservation works by sending a `reserve` request to the worker, and the
+/// worker will respond with a token if it is not reserved. A subsequent `work`
+/// request must include the token to be accepted; by the end of the work, the
+/// worker will release the reservation. If the worker does not receive a
+/// `work` request within a [`token::TIMEOUT`], the worker will release the
+/// reservation automatically.
 pub struct Worker<T, R, F, FO, E>
 where
     T: DeserializeOwned + Serialize + std::marker::Sync + std::marker::Send,
